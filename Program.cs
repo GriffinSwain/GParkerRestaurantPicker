41d50fc418cca89783c4415c109cbcdfe9102c25
Griffin <116022056+GriffinSwain@users.noreply.github.com>
2022-10-20 15:10:12 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@
 //Project # 3 - Restaurant Picker
 //This program gives the user to choose from three different types of restaurants and then gives them a random restaurant that they should go to.
 //Peer Reviewed by Reed Goodwin & Mark Ramirez
+//Mark: works good validation is great and i want to know how you do the little animation thingyeyys's
+//Reed: I think he did a great job with the UI, it has a intertaining animation and cool feature to give u a drumroll for your answer. Everything works as intended, 
+//i would have added a 4rth choice to grab a totally random one. Other than that its perfect!
 
 int choice = 0;
 int num = 0;
