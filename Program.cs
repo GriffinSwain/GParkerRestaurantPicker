aeb992f7fb42db2705b69cd551c5e34347f65cfb
Griffin <116022056+GriffinSwain@users.noreply.github.com>
2022-10-20 14:56:06 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 //October 20. 2022
 //Project # 3 - Restaurant Picker
 //This program gives the user to choose from three different types of restaurants and then gives them a random restaurant that they should go to.
+//Peer Reviewed by Reed Goodwin & Mark Ramirez
 
 int choice = 0;
 int num = 0;
